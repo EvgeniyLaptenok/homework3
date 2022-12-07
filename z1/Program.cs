5261185c308de617a17cbd9b292df7f9cafefa61
evgeniy <shtorm20300@gmail.com>
2022-12-07 23:44:21 +0600

    палиндром

---
+++ z1/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите пятизначное число: ");
+int number = int.Parse(Console.ReadLine()!);
+int num = Math.Abs(number);
+int firstPair = num / 10000 + ((num / 1000) % 10 * 10);
+int secondPair = num % 100;
+
+if (num >= 100000 || num <= 9999)
+{
+    Console.WriteLine("Неверное число");
+}
+else
+{
+    if (firstPair == secondPair)
+    {
+        Console.WriteLine("Палидром");
+    }
+    else
+    {
+        Console.WriteLine("Не палидром");
+    }
+}
