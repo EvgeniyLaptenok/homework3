2971185ffee6f18c4c0728e4b0f873c5980696e9
evgeniy <shtorm20300@gmail.com>
2022-12-08 00:01:52 +0600

    куб числа

---
+++ z3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+
+for (int i = 1; i <= N; i++)
+{
+    double sqr = Math.Pow(i, 3);
+    Console.Write(sqr + ", ");
+}
+Console.WriteLine();
